f5620c8cd8367d1755eb2a24d1a9d7f1d9a1f053
Olamide <olire96@gmail.com>
2022-12-06 18:00:43 +0100

    pop

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
     string_list = list(my_string)
-    list_len = len(string_list)
-    for i in range(list_len):
-        if list_len > len(string_list):
+    for i in range(len(string_list)):
+        if i == len(string_list):
             break
-        elif string_list[i] == 'c' or string_list[i] == 'C':
+        if string_list[i] == 'c' or string_list[i] == 'C':
             string_list.pop(i)
     new_string = "".join(string_list)
     return new_string
