5de9cb5d3571172a1af53329bb807651d4d80b94
Olamide <olire96@gmail.com>
2023-01-02 16:45:28 +0100

    first task

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""A class that creates a rectangle"""
+
+class Rectangle:
+    """A class for a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This is the README file for the more python classes module.
