0f66b4c87ea82519e86e939ec87b4a1be84d5149
Olamide <olire96@gmail.com>
2023-01-09 21:05:09 +0100

    check muizzy

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -18,4 +18,4 @@ def inherits_from(obj, a_class):
     - bool: True if the object is an instance of a class that inherited
     (directly or indirectly) from the specified class; otherwise False
     """
-    return type(obj) != a_class
+    return type(obj) != a_class and isinstance(obj, a_class)
