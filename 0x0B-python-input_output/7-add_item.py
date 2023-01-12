a243a17300c3ac5ae5c0fec4936de9b6c9ad2ced
Olamide <olire96@gmail.com>
2023-01-12 19:09:05 +0100

    module documentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""A script to fill a json file"""
+
 import sys
 import json
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_fil
-"""A script to fill a json file"""
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 try:
