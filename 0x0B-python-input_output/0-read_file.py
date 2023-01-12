0a0db2e326f7620dba5ffa079e02e31e148f64b5
Olamide <olire96@gmail.com>
2023-01-12 17:36:09 +0100

    first task

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for a function to read a file"""
+
+
+def read_file(filename=""):
+    """
+    This function reads a text file and prints its contents to stdout.
+
+    Parameters:
+    filename (str): The file path of the file to be read.
+
+    Returns:
+    None
+    """
+    with open(filename, 'r', encoding='utf8') as f:
+        print(f.read())
