7d2bd00e76113298960ccfc004ab42ac1eea795a
Olamide <olire96@gmail.com>
2023-02-15 20:17:30 +0100

    show tables

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- Create a database if it doesn't exist
+
+-- The command to do so.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- Delete a database if it exists
+
+-- The implementation
+DROP DATABASE IF EXISTS hbtn_0c_0;
