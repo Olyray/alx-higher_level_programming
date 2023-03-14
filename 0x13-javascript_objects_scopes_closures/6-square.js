1361eb2ef788f963dcb51a9b9bf140be68a7626a
Olamide <olire96@gmail.com>
2023-03-14 22:51:12 +0100

    semistandard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
-module.exports = class Square extends require ('./5-square.js') {
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
-  if (c === undefined) {
-    this.print();
-  } else {
-    for (let i = 0; i < this.height; i++) {
-      for (let i = 0; i < this.width; i++) {
-        process.stdout.write ('C');
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let i = 0; i < this.width; i++) {
+          process.stdout.write('C');
+        }
+        console.log();
       }
-      console.log();
     }
-  }
   }
 };
