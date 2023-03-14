dd956a5efe7eaf073d6071efe2ce8d2f6c9c325b
Olamide <olire96@gmail.com>
2023-03-14 22:52:19 +0100

    C

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,7 +6,7 @@ module.exports = class Square extends require('./5-square.js') {
     } else {
       for (let i = 0; i < this.height; i++) {
         for (let i = 0; i < this.width; i++) {
-          process.stdout.write('C');
+          process.stdout.write(c);
         }
         console.log();
       }
