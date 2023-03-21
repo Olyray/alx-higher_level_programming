9352ea7114ceb214835bd3a256bf7dbfe76ae183
Olamide <olire96@gmail.com>
2023-03-21 18:08:10 +0100

    pycodestyle

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from model_state import Base
 
+
 class City(Base):
     """
     The implementation of the Base model
