a13525642508cdb38c32046979e64b90d0e45c49
Olamide <olire96@gmail.com>
2023-03-21 17:48:26 +0100

    the rest

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Prints the first item in the States table
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_in_var = (session.query(State).filter(State.name.like(sys.argv[4]))
+                    .order_by(State.id).all())
+    if not state_in_var:
+        print("Not found")
+    else:
+        print(state_in_var[0].id)
---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Prints the first item in the States table
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_to_update = session.get(State, 2)
+    state_to_update.name = "New Mexico"
+    session.commit()
+    session.close()
