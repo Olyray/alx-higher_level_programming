b1f965de21fdd7968e838f1a995b297321c78a30
Olamide <olire96@gmail.com>
2023-03-31 16:52:59 -0700

    number 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Check for valid JSON"""
+import requests
+from sys import argv
+import json
+
+
+if __name__ == "__main__":
+    breakpoint()
+    if len(argv) == 1:
+        letter = {"q": ""}
+    else:
+        letter = {"q": argv[1]}
+    response = requests.post("http://0.0.0.0:5000/search_user", data=letter)
+    try:
+        data = response.json()
+        if data == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(data.get("id"), data.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
